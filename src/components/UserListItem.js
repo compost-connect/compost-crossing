10655a99434bba0cc5c2eec2ac47491e7b667c26
John Mandalakas <john@mandalakas.com>
2016-06-04 17:03:27 -0500

    Add icons

--- src/components/UserListItem.js
+++ src/components/UserListItem.js
@@ -1,10 +1,12 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
+import { getIcon } from '../utils/Utils';
 
-const UserListItem = ({name, email, refuseInfo, logisticInfo}) => {
+const UserListItem = ({name, participant_type, email, refuseInfo, logisticInfo}) => {
   return (
     <li>
-      <img src="http://0.gravatar.com/avatar/decbc5a867fb73b9c8f2dd2731b385be"/>
+      <img className="profile-pic" src="http://0.gravatar.com/avatar/decbc5a867fb73b9c8f2dd2731b385be"/>
+      <img className="participant-icon" src={getIcon(participant_type)} />
       <span className="user-name">{name}</span>
       <p>Food scraps, coffee grounds</p>
       <p>Receive pickup, Drop off</p>
