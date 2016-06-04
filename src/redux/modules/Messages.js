0ab566e54e2e93d3b8ee8e233453e892b7d7dc85
John Mandalakas <john@mandalakas.com>
2016-06-04 18:18:05 -0500

    Fix error by adding needed fn

--- src/redux/modules/Messages.js
+++ src/redux/modules/Messages.js
@@ -1,6 +1,13 @@
 const SEND_MESSAGE = 'SEND_MESSAGE';
 const NOTIFY_USER = 'NOTIFY_USER';
+const FETCH_MESSAGES = 'FETCH_MESSAGES'
 
 export default function reducer(state = [], action) {
   return state;
+}
+
+export function checkForMessages() {
+  return {
+    type: FETCH_MESSAGES
+  };
 }
