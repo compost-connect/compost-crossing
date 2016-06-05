1776649cc13d24737e79293b0c0724c1e014fd42
John Mandalakas <john@mandalakas.com>
2016-06-05 03:20:42 -0500

    Get the questionnaire data directly

--- src/components/UserListItem.js
+++ src/components/UserListItem.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import { getIcon } from '../utils/Utils';
 
-const UserListItem = ({name, participant_type, avatar_url, email, refuseInfo, logisticInfo}) => {
+const UserListItem = ({name, participant_type, avatar_url, email, questionnaire={fields:[]}}) => {
   return (
     <li>
       <img
@@ -14,10 +14,16 @@ const UserListItem = ({name, participant_type, avatar_url, email, refuseInfo, lo
         className="participant-icon" src={getIcon(participant_type)} />
       <span className="user-name">{name}</span>
       <p key="refuse-info">
-        Food scraps, coffee grounds
+        {
+          questionnaire.fields.filter(field => !!field.refuse)[0] ?
+          questionnaire.fields.filter(field => !!field.refuse)[0].refuse.join(', ') :
+          ''}
       </p>
       <p key="logistic-info">
-        Receive pickup, Drop off
+        {
+          questionnaire.fields.filter(field => !!field.logistic)[0] ?
+          questionnaire.fields.filter(field => !!field.logistic)[0].logistic :
+          ''}
       </p>
       <Link to={`send-message/${email}`}>
         <button>Message</button>
