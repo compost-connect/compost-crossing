30ff837b39f4142f51acb2afb2c557ba0316c259
John Mandalakas <john@mandalakas.com>
2016-06-05 03:38:10 -0500

    Add default pic and use real text

--- src/components/UserAvatar.js
+++ src/components/UserAvatar.js
@@ -7,7 +7,7 @@ const UserAvatar = ({avatar_url, participant_type}) => {
       <img
         key="profile-image"
         className="profile-pic"
-        src={avatar_url}/>
+        src={avatar_url || "http://compost-crossing.herokuapp.com/public/images/unknown_avatar.png"}/>
       <img
         key="icon-image"
         className="participant-icon" src={getIcon(participant_type)} />
--- src/components/UserListItem.js
+++ src/components/UserListItem.js
@@ -1,17 +1,12 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import { getIcon } from '../utils/Utils';
+import UserAvatar from './UserAvatar';
 
 const UserListItem = ({name, participant_type, avatar_url, email, questionnaire={fields:[]}}) => {
   return (
     <li>
-      <img
-        key="profile-image"
-        className="profile-pic"
-        src={avatar_url}/>
-      <img
-        key="icon-image"
-        className="participant-icon" src={getIcon(participant_type)} />
+      <UserAvatar participant_type={participant_type} avatar_url={avatar_url} />
       <span className="user-name">{name}</span>
       <p key="refuse-info">
         {
@@ -23,7 +18,8 @@ const UserListItem = ({name, participant_type, avatar_url, email, questionnaire=
         {
           questionnaire.fields.filter(field => !!field.logistic)[0] ?
           questionnaire.fields.filter(field => !!field.logistic)[0].logistic :
-          ''}
+          ''
+        }
       </p>
       <Link to={`send-message/${email}`}>
         <button>Message</button>
